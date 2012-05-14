c8b9090a2013d6d33e017c229831a9e689dd8150
Keenan Wyrobek <keenan@openreading.org>
2012-05-13 23:57:38 -0700

    removed extra white space in functions php that was breaking rss xml syntax

--- functions.php
+++ functions.php
@@ -9,4 +9,11 @@ function insertThumbnailRSS($content) {
 }
 add_filter('the_excerpt_rss', 'insertThumbnailRSS');
 add_filter('the_content_feed', 'insertThumbnailRSS');
+
+// Puts link in excerpts more tag
+function excerpt_read_more_link($output) {
+ global $post;
+ return $output . '<a href="'. get_permalink($post->ID) . '"> Read More</a>';
+}
+add_filter('the_excerpt', 'excerpt_read_more_link');
 ?>
